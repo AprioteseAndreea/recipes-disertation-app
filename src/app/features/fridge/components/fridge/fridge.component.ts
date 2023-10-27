69ee45d9b0e7e78524a2cb7906087c72cb3cb6cf
AprioteseAndreea <79917865+AprioteseAndreea@users.noreply.github.com>
2023-10-27 21:47:02 +0300

    Added some frontend work

--- src/app/features/fridge/components/fridge/fridge.component.ts
+++ src/app/features/fridge/components/fridge/fridge.component.ts
@@ -1,25 +1,35 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MdbModalRef, MdbModalService } from 'mdb-angular-ui-kit/modal';
 import { AddItemComponentComponent } from '../../modals/add-item-component/add-item-component.component';
 import { NotificationService } from 'src/app/core/services/notification.service';
 import { ToastContainerDirective, ToastrService } from 'ngx-toastr';
 import constants from 'src/app/core/constants/constants';
+import { AccountService } from 'src/app/features/auth/services/account.service';
+import { UserIngredient } from 'src/app/core/models/user.model';
+import { environment } from 'src/app/core/environments/environment';
 
 @Component({
   selector: 'app-fridge',
   templateUrl: './fridge.component.html',
   styleUrls: ['./fridge.component.scss'],
 })
-export class FridgeComponent {
+export class FridgeComponent implements OnInit{
   ingredients = constants.Ingredients;
 
   @ViewChild(ToastContainerDirective, { static: true })
   modalRef: MdbModalRef<AddItemComponentComponent> | null = null;
+  fridgeData: UserIngredient[];
 
   constructor(
     private modalService: MdbModalService,
-    private notificationService: NotificationService
+    private notificationService: NotificationService,
+    public accountService: AccountService
   ) {}
+  ngOnInit(): void {
+    this.fridgeData = this.accountService.loggedUserValue.userIngredients.filter(
+      (ingredient) => ingredient.isCartIngredient === false
+    );
+  }
 
   openDialog() {
     this.modalRef = this.modalService.open(AddItemComponentComponent, {
@@ -30,4 +40,8 @@ export class FridgeComponent {
     });
   }
   refreshData() {}
+
+  getIngredientUrl(id: string) {
+    return `${environment.apiUrl}/images/${id}`;
+  }
 }
